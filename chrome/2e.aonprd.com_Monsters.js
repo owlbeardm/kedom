9dadff7e23ffd2f5f3294e146ba8ca2f222c3969
Mikita Kukavenka <godinfrog@gmail.com>
2021-03-17 19:31:11 +0300

    chrome extention

---
+++ chrome/2e.aonprd.com_Monsters.js
@@ -0,0 +1,106 @@
+console.log("Pathfinder proficiency without level for 2e.aonprd.com/Monsters");
+
+// SIMPLE DC
+let sdLoop = true;
+
+function replaceSimpleDc(node) {
+  if (!sdLoop) return;
+  const caption = node.querySelectorAll("h4");
+  if (!caption) return;
+  if (
+    caption[0] &&
+    caption[0].outerText &&
+    caption[0].outerText.toLowerCase().includes("simple dc")
+  ) {
+    const textNodes = node.querySelectorAll(".text-center");
+    if (textNodes) {
+      textNodes.forEach((textNode) => {
+        replaceInnerNode(textNode, "30", "25");
+        replaceInnerNode(textNode, "40", "30");
+      });
+    }
+    sdLoop = false;
+  }
+}
+// const digit = /(?<=[+-])\d+/;
+const digit = /(?<!\d)[+-]\d+/;
+function sDC() {
+  setTimeout(function () {
+    let main = document.querySelectorAll("#main");
+    if (main && main[0]) {
+      const creatures = main[0].querySelectorAll("h1>span");
+      let level = undefined;
+      creatures.forEach((creature) => {
+        if (level || level === 0) return;
+        console.log(creature.innerHTML);
+        level = creature.innerHTML.match(/\d+/)[0];
+        if (level) {
+          level = level * 1.0;
+          console.log(
+            "Pathfinder proficiency without level, creature level",
+            level
+          );
+        }
+      });
+      if (level || level === 0) {
+        sdLoop = false;
+        console.log("Pathfinder proficiency without level", main, main[0]);
+        let matched = true;
+        let start = 0;
+        while (matched) {
+          const match = main[0].innerHTML.substring(start).match(digit);
+
+          if (!match) {
+            matched = false;
+          } else {
+            const searchIndex = main[0].innerHTML
+              .substring(start)
+              .search(digit);
+            const index = searchIndex + 66 + 2 * match[0].length;
+            if (searchIndex > 0)
+              replaceInnerNode(
+                main[0],
+                start + searchIndex,
+                match[0].length,
+                match[0],
+                level
+              );
+            start = searchIndex > 0 ? start + index : main[0].innerHTML.length;
+            console.log("match", match[0], start);
+          }
+        }
+        // select.forEach(replaceSimpleDc);
+        // select.forEach(replaceSpellDc);
+        // select.forEach(replaceDcByLevel);
+      }
+    }
+    if (sdLoop) {
+      console.log("Pathfinder proficiency without level again");
+      sDC();
+    }
+  }, 1000);
+}
+sDC();
+
+function replaceInnerNode(node, from, length, original, level) {
+  console.log(
+    "Pathfinder proficiency without level again",
+    node,
+    from,
+    length,
+    original,
+    level,
+    original * 1.0 - level
+  );
+  console.log(
+    "Pathfinder proficiency without level again",
+    node.innerHTML.substring(from - 20, from + 20)
+  );
+  const to = original * 1.0 - level;
+  node.innerHTML =
+    node.innerHTML.substring(0, from) +
+    `<span style="border-bottom: 1px dotted;" title="${original} original">${
+      to > 0 ? "+" + to : to
+    }</span>` +
+    node.innerHTML.substring(from + length);
+}
---
+++ chrome/character.pf2.tools.js
@@ -0,0 +1,99 @@
+console.log("Pathfinder proficiency without level for character.pf2.tools");
+
+// SIMPLE DC
+let sdLoop = true;
+function replaceSimpleDc(node) {
+  if (!sdLoop) return;
+  const caption = node.querySelectorAll("h4");
+  if(!caption) return;
+  if (caption[0] && caption[0].outerText && caption[0].outerText.toLowerCase().includes("simple dc")) {
+    const textNodes = node.querySelectorAll(".text-center");
+    if(textNodes){
+        textNodes.forEach((textNode)=>{
+            replaceInnerNode(textNode, "30", "25");
+            replaceInnerNode(textNode, "40", "30");
+        });
+    }
+    sdLoop = false;
+  }
+}
+function replaceSpellDc(node) {
+    if (!sdLoop) return;
+    const caption = node.querySelectorAll("h4");
+    if(!caption) return;
+    if (caption[0] && caption[0].outerText && caption[0].outerText.toLowerCase().includes("spell level dcs")) {
+      const textNodes = node.querySelectorAll(".text-center");
+      if(textNodes){
+          textNodes.forEach((textNode)=>{
+              replaceInnerNode(textNode, "18", "16");
+              replaceInnerNode(textNode, "20", "16");
+              replaceInnerNode(textNode, "23", "17");
+              replaceInnerNode(textNode, "26", "18");
+              replaceInnerNode(textNode, "28", "18");
+              replaceInnerNode(textNode, "31", "19");
+              replaceInnerNode(textNode, "34", "20");
+              replaceInnerNode(textNode, "36", "20");
+              replaceInnerNode(textNode, "39", "21");
+          });
+      }
+      sdLoop = false;
+    }
+  }
+function replaceDcByLevel(node) {
+    const caption = node.querySelectorAll("h4");
+    if(!caption) return;
+    if (caption[0] && caption[0].outerText && caption[0].outerText.toLowerCase().includes("dc by level")) {
+      const textNodes = node.querySelectorAll(".text-center");
+      if(textNodes){
+          textNodes.forEach((textNode)=>{
+              replaceInnerNode(textNode, "14", "15");
+              replaceInnerNode(textNode, "16", "15");
+              replaceInnerNode(textNode, "18", "16");
+              replaceInnerNode(textNode, "19", "16");
+              replaceInnerNode(textNode, "20", "16");
+              replaceInnerNode(textNode, "22", "17");
+              replaceInnerNode(textNode, "23", "17");
+              replaceInnerNode(textNode, "24", "17");
+              replaceInnerNode(textNode, "26", "18");
+              replaceInnerNode(textNode, "27", "18");
+              replaceInnerNode(textNode, "28", "18");
+              replaceInnerNode(textNode, "30", "19");
+              replaceInnerNode(textNode, "31", "19");
+              replaceInnerNode(textNode, "32", "19");
+              replaceInnerNode(textNode, "34", "20");
+              replaceInnerNode(textNode, "35", "20");
+              replaceInnerNode(textNode, "36", "20");
+              replaceInnerNode(textNode, "38", "21");
+              replaceInnerNode(textNode, "39", "21");
+              replaceInnerNode(textNode, "40", "21");
+              replaceInnerNode(textNode, "42", "22");
+              replaceInnerNode(textNode, "44", "23");
+              replaceInnerNode(textNode, "46", "24");
+              replaceInnerNode(textNode, "48", "25");
+              replaceInnerNode(textNode, "50", "26");
+          });
+      }
+    }
+  }
+function sDC() {
+  setTimeout(function () {
+    let select = document.querySelectorAll(".card-body .col div");
+    select.forEach(replaceSimpleDc);
+    select.forEach(replaceSpellDc);
+    select.forEach(replaceDcByLevel);
+    if (sdLoop) {
+      console.log(
+        "Pathfinder proficiency without level for character.pf2.tools sDC again"
+      );
+      sDC();
+    }
+  }, 5000);
+}
+sDC();
+
+function replaceInnerNode(node, from, to) {
+  node.innerHTML = node.innerHTML.replaceAll(
+    from,
+    `<span style="border-bottom: 1px dotted;" title="${from} original">${to}</span>`
+  );
+}
