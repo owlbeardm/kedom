a869501cf9ed6e2ec1ca37589998fb02e7d34e9a
c7d5a6 <godinfrog@gmail.com>
2021-07-15 23:05:06 +0300

    something

--- chrome/character.pf2.tools.js
+++ chrome/character.pf2.tools.js
@@ -5,76 +5,75 @@ let sdLoop = true;
 function replaceSimpleDc(node) {
   if (!sdLoop) return;
   const caption = node.querySelectorAll("h4");
-  if(!caption) return;
+  if (!caption) return;
   if (caption[0] && caption[0].outerText && caption[0].outerText.toLowerCase().includes("simple dc")) {
     const textNodes = node.querySelectorAll(".text-center");
-    if(textNodes){
-        textNodes.forEach((textNode)=>{
-            replaceInnerNode(textNode, "30", "25");
-            replaceInnerNode(textNode, "40", "30");
-        });
+    if (textNodes) {
+      textNodes.forEach((textNode) => {
+        replaceInnerNode(textNode, "30", "25");
+        replaceInnerNode(textNode, "40", "30");
+      });
     }
-    sdLoop = false;
+
   }
 }
 function replaceSpellDc(node) {
-    if (!sdLoop) return;
-    const caption = node.querySelectorAll("h4");
-    if(!caption) return;
-    if (caption[0] && caption[0].outerText && caption[0].outerText.toLowerCase().includes("spell level dcs")) {
-      const textNodes = node.querySelectorAll(".text-center");
-      if(textNodes){
-          textNodes.forEach((textNode)=>{
-              replaceInnerNode(textNode, "18", "16");
-              replaceInnerNode(textNode, "20", "16");
-              replaceInnerNode(textNode, "23", "17");
-              replaceInnerNode(textNode, "26", "18");
-              replaceInnerNode(textNode, "28", "18");
-              replaceInnerNode(textNode, "31", "19");
-              replaceInnerNode(textNode, "34", "20");
-              replaceInnerNode(textNode, "36", "20");
-              replaceInnerNode(textNode, "39", "21");
-          });
-      }
-      sdLoop = false;
+  if (!sdLoop) return;
+  const caption = node.querySelectorAll("h4");
+  if (!caption) return;
+  if (caption[0] && caption[0].outerText && caption[0].outerText.toLowerCase().includes("spell level dcs")) {
+    const textNodes = node.querySelectorAll(".text-center");
+    if (textNodes) {
+      textNodes.forEach((textNode) => {
+        replaceInnerNode(textNode, "18", "16");
+        replaceInnerNode(textNode, "20", "16");
+        replaceInnerNode(textNode, "23", "17");
+        replaceInnerNode(textNode, "26", "18");
+        replaceInnerNode(textNode, "28", "18");
+        replaceInnerNode(textNode, "31", "19");
+        replaceInnerNode(textNode, "34", "20");
+        replaceInnerNode(textNode, "36", "20");
+        replaceInnerNode(textNode, "39", "21");
+      });
     }
   }
+}
 function replaceDcByLevel(node) {
-    const caption = node.querySelectorAll("h4");
-    if(!caption) return;
-    if (caption[0] && caption[0].outerText && caption[0].outerText.toLowerCase().includes("dc by level")) {
-      const textNodes = node.querySelectorAll(".text-center");
-      if(textNodes){
-          textNodes.forEach((textNode)=>{
-              replaceInnerNode(textNode, "14", "15");
-              replaceInnerNode(textNode, "16", "15");
-              replaceInnerNode(textNode, "18", "16");
-              replaceInnerNode(textNode, "19", "16");
-              replaceInnerNode(textNode, "20", "16");
-              replaceInnerNode(textNode, "22", "17");
-              replaceInnerNode(textNode, "23", "17");
-              replaceInnerNode(textNode, "24", "17");
-              replaceInnerNode(textNode, "26", "18");
-              replaceInnerNode(textNode, "27", "18");
-              replaceInnerNode(textNode, "28", "18");
-              replaceInnerNode(textNode, "30", "19");
-              replaceInnerNode(textNode, "31", "19");
-              replaceInnerNode(textNode, "32", "19");
-              replaceInnerNode(textNode, "34", "20");
-              replaceInnerNode(textNode, "35", "20");
-              replaceInnerNode(textNode, "36", "20");
-              replaceInnerNode(textNode, "38", "21");
-              replaceInnerNode(textNode, "39", "21");
-              replaceInnerNode(textNode, "40", "21");
-              replaceInnerNode(textNode, "42", "22");
-              replaceInnerNode(textNode, "44", "23");
-              replaceInnerNode(textNode, "46", "24");
-              replaceInnerNode(textNode, "48", "25");
-              replaceInnerNode(textNode, "50", "26");
-          });
-      }
+  const caption = node.querySelectorAll("h4");
+  if (!caption) return;
+  if (caption[0] && caption[0].outerText && caption[0].outerText.toLowerCase().includes("dc by level")) {
+    const textNodes = node.querySelectorAll(".text-center");
+    if (textNodes) {
+      textNodes.forEach((textNode) => {
+        replaceInnerNode(textNode, "14", "15");
+        replaceInnerNode(textNode, "16", "15");
+        replaceInnerNode(textNode, "18", "16");
+        replaceInnerNode(textNode, "19", "16");
+        replaceInnerNode(textNode, "20", "16");
+        replaceInnerNode(textNode, "22", "17");
+        replaceInnerNode(textNode, "23", "17");
+        replaceInnerNode(textNode, "24", "17");
+        replaceInnerNode(textNode, "26", "18");
+        replaceInnerNode(textNode, "27", "18");
+        replaceInnerNode(textNode, "28", "18");
+        replaceInnerNode(textNode, "30", "19");
+        replaceInnerNode(textNode, "31", "19");
+        replaceInnerNode(textNode, "32", "19");
+        replaceInnerNode(textNode, "34", "20");
+        replaceInnerNode(textNode, "35", "20");
+        replaceInnerNode(textNode, "36", "20");
+        replaceInnerNode(textNode, "38", "21");
+        replaceInnerNode(textNode, "39", "21");
+        replaceInnerNode(textNode, "40", "21");
+        replaceInnerNode(textNode, "42", "22");
+        replaceInnerNode(textNode, "44", "23");
+        replaceInnerNode(textNode, "46", "24");
+        replaceInnerNode(textNode, "48", "25");
+        replaceInnerNode(textNode, "50", "26");
+      });
     }
   }
+}
 function sDC() {
   setTimeout(function () {
     let select = document.querySelectorAll(".card-body .col div");
@@ -92,8 +91,11 @@ function sDC() {
 sDC();
 
 function replaceInnerNode(node, from, to) {
-  node.innerHTML = node.innerHTML.replaceAll(
-    from,
-    `<span style="border-bottom: 1px dotted;" title="${from} original">${to}</span>`
-  );
+  if (node.innerHTML.replaceAll) {
+    node.innerHTML = node.innerHTML.replaceAll(
+      from,
+      `<span style="border-bottom: 1px dotted;" title="${from} original">${to}</span>`
+    );
+    sdLoop = false;
+  }
 }
